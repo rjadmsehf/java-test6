7ccf591a00bd91c6f2657036db53ec5442043ea8
rjadmsehf <jjg524@naver.com>
2024-02-10 20:10:32 +0900

    테스트

---
+++ src/main/java/com/ohgiraffers/section01/conditional/test1.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.section01.conditional;
+
+public class test1 {
+
+    public static void main(String[] args) {
+
+
+
+
+        test a = new test();
+        a.test1();
+
+
+    }
+}
